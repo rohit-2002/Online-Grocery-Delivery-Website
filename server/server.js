a74bcc77c64dd845b232d237d6c6bd26fbef3d2f
rohit-2002 <rohitbaba198@gmail.com>
2025-07-04 22:20:29 +0530

    Added the frontend url

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://freshbasket-backend.vercel.app",
+  "https://freshbasket-frontend-seven.vercel.app",
 ];
 app.use(
   cors({
