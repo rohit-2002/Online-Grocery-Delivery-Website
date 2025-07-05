0c677c9b259d09cf1c12e7f6fe9a5bda73370a49
rohit-2002 <rohitbaba198@gmail.com>
2025-07-05 09:17:21 +0530

    Changes in sellerLayout

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -4,7 +4,7 @@ import { assets } from "../../assets/assets";
 import { NavLink, Outlet, Link } from "react-router-dom";
 import toast from "react-hot-toast";
 const SellerLayout = () => {
-  const { axios, navigate } = useAppContext();
+  const { axios, navigate, setIsSeller } = useAppContext();
 
   const sidebarLinks = [
     { name: "Add Product", path: "/seller", icon: assets.add_icon },
@@ -21,6 +21,7 @@ const SellerLayout = () => {
       const { data } = await axios.get("/api/seller/logout");
       if (data.success) {
         toast.success(data.message);
+        setIsSeller(false);
         navigate("/");
       } else {
         toast.error(data.message);
